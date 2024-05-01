28953938fc6f80670f3eea6485161eb6a2c01ac4
janmurer <jan.murer@bluewin.ch>
2024-05-01 11:09:09 +0200

    Load API-key

--- 03_scripts/functions.R
+++ 03_scripts/functions.R
@@ -1,10 +1,13 @@
+library(tidyverse)
 library(here)
+
 
 # create wrapper function to get coordinates from opencage api.
 
 # load api key from .csv using here
 
-api_key_opencage <- read.csv(here(""))
+api_key_opencage <- read.csv(here("00_api_credentials/api_crentials.csv") %>%
+                               pull(geocoding_api_key)
 
 
 
