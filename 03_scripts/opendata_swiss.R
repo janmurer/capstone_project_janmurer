7fffa137f3a87dde9fd4afca7a89a1bc5d49116f
janmurer <jan.murer@bluewin.ch>
2024-05-02 09:56:19 +0200

    minor formal adjustements

--- 03_scripts/opendata_swiss.R
+++ 03_scripts/opendata_swiss.R
@@ -1,17 +1,30 @@
 library(jsonlite)
 library(here)
+library(dplyr)
 
-# Load data using fromJSON and here
+# load data using fromJSON and here
 opendata_swiss_toilettes <- fromJSON(here("01_data_input", "TOILETTE.json"))
 
-# Create a new list to store combined properties and coordinates
+# create a new list to store combined properties and coordinates
+
 opendata_swiss_toilettes_combine_coordinates_features <- lapply(1:nrow(opendata_swiss_toilettes$features), function(i) {
-  # Extract properties and coordinates
+ 
+   # extract properties and coordinates
+  
   properties <- opendata_swiss_toilettes$features$properties[i, ]
   coordinates <- opendata_swiss_toilettes$features$geometry$coordinates[[i]]  # Access coordinates list
   
-  # Combine properties and coordinates into a single structure
+  # combine properties and coordinates into a single structure
+  
   combined <- list(properties = properties, coordinates = coordinates)
   
   return(combined)
 })
+
+
+# convert the list of lists into a dataframe
+
+opendata_swiss_toilettes_df <- bind_rows(opendata_swiss_toilettes_combine_coordinates_features)
+
+
+
