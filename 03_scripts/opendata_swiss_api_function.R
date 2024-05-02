35e575bda9deca3fa18d2fec57bcf33ef4188de2
janmurer <jan.murer@bluewin.ch>
2024-05-02 08:59:04 +0200

    remove json parsing from opendata.swiss function as it is not needed.

--- 03_scripts/opendata_swiss_api_function.R
+++ 03_scripts/opendata_swiss_api_function.R
@@ -1,48 +1,45 @@
 library(httr)
-library(jsonlite)
 library(here)
 
-# Function to fetch data from opendata.swiss API
+# function to fetch data from opendata.swiss API
 
 fetch_opendata_swiss <- function(dataset_id) {
-  # Construct API request URL
+  
+  # construct API request URL
   
   url <- paste0("https://opendata.swiss/api/3/action/package_show?id=", dataset_id)
   
-  # Make GET request
+  # make GET request
   
   response <- GET(url)
   
-  # Check if request was successful
+  # check if request was successful
   
   if (http_type(response) == "application/json") {
     
-    # Parse JSON response
+    # get the resource URL directly from the JSON response
     
     content <- content(response, "text")
     json_content <- fromJSON(content)
     
-    # Check if resources are available
+    # check if resources are available
     
     if (!is.null(json_content$result$resources)) {
       
-      # Find the resource URL for the dataset you want to download
-      
-      # You may need to adjust this based on the structure of the JSON response
+      # find the resource URL for the dataset you want to download
       
       resource_url <- json_content$result$resources$url[1]  # Assuming the first resource is the dataset
       
-      # Download the dataset
+      # download the dataset
       
       dataset <- GET(resource_url)
       
-      # Save the dataset to a temporary file
+      # save the dataset to a temporary file
       
       temp_zip <- tempfile(fileext = ".zip")
       writeBin(content(dataset, "raw"), temp_zip)
       
-      # Unzip the dataset to "01_data_input" folder using here package
-      
+      # unzip the dataset to "01_data_input" folder using base R function
       unzip(temp_zip, exdir = here("01_data_input"))
       
       return(paste("Dataset downloaded and extracted to '01_data_input' folder"))
@@ -53,7 +50,6 @@ fetch_opendata_swiss <- function(dataset_id) {
     return("Failed to fetch data.")
   }
 }
-
 
 dataset_id <- "0d56b6ae-4db2-4080-a958-4fb54b11c988"
 fetch_opendata_swiss(dataset_id)
