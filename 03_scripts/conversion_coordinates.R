87df1c6336e2b33269a1220cd38bbf615b31c0d0
janmurer <jan.murer@bluewin.ch>
2024-05-03 13:16:29 +0200

    convert swiss coordinates to longitude latitude

---
+++ 03_scripts/conversion_coordinates.R
@@ -0,0 +1,22 @@
+library(sf)
+
+# Create an sf object with the coordinates
+coords_sf <- st_as_sf(opendata_swiss_toilettes_df_cleaned, coords = c("x_coord", "y_coord"))
+
+# Set the current CRS to EPSG:2056
+st_crs(coords_sf) <- 2056
+
+# Transform coordinates to EPSG:4326 (WGS 84)
+coords_sf_wgs84 <- st_transform(coords_sf, crs = 4326)
+
+# Extract transformed coordinates
+opendata_swiss_toilettes_df_cleaned <- cbind(opendata_swiss_toilettes_df_cleaned, st_coordinates(coords_sf_wgs84))
+
+# Rename the new columns
+colnames(opendata_swiss_toilettes_df_cleaned)[6:7] <- c("longitude", "latitude")
+
+# Drop the original coordinate columns
+opendata_swiss_toilettes_df_cleaned_international_coordinates <- opendata_swiss_toilettes_df_cleaned %>% 
+  select(-x_coord, -y_coord)
+
+
