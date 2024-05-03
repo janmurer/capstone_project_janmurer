3fa65a63616afba2287452ec318c6d7cae9c31bc
janmurer <jan.murer@bluewin.ch>
2024-05-03 15:22:06 +0200

    automatically set up r environnement

--- 03_scripts/main.R
+++ 03_scripts/main.R
@@ -1,11 +1,16 @@
+# Run functions.R and opendata_swiss to setup R-environnement
+
+source("03_scripts/functions.R")
+source("03_scripts/opendata_swiss.R")
+
 # Define your location
 
-define_user_location("Kreuzbuchstrasse 38, 6006 Luzern, Switzerland")
+define_user_location("Hochrütistrasse 35, 6005 Luzern, Switzerland")
 
-# Get your exact coordinates
+# Fetch your exact coordinates and store them in a variable
 
 user_coordinates <- get_user_coordinates()
 
-# Find the nearest public toilet
+# Find the nearest public toilet printed in the console. 
 
 find_nearest_location(user_coordinates, opendata_swiss_toilettes_df_cleaned_international_coordinates)
